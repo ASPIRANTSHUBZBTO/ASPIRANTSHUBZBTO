ed72f7e434b3e2fea99f673e8effedc9d0a5ae05
Sai <ASPIRANTSHUBZBTO@gmail.com>
2026-03-21 21:34:57 +0530

    Changes to Submit link and clearing commented code

--- script.js
+++ script.js
@@ -12,8 +12,9 @@ document.getElementById("form").addEventListener("submit", function(e) {
         qualification: e.target[3].value
     };
 
-    fetch("https://script.google.com/macros/s/AKfycbwV2ThmHl681EDt1D2VBOYpo0t22hy5tHPuVVX2UlJ7As0_1j3_1SNBnVtUSOzydCNo/exec", {
+    fetch("https://script.google.com/macros/s/AKfycbxHsZ8dA2j2YgQ-WKwjgFhDledijhshTUyrk1X79PLDtKCoSKppJjvnD8asQilhHn1l9w/exec", {
         method: "POST",
+        mode: "no-cors",
         body: JSON.stringify(data)
     })
     .then(res => res.text())
@@ -23,39 +24,6 @@ document.getElementById("form").addEventListener("submit", function(e) {
     })
     .catch(() => alert("Error submitting form"));
 });
-
-
-// const mentorsContainer = document.getElementById("mentors-list");
-
-// // 🔁 Replace with your deployed URL
-// const API_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQjjPFy4GO0YnuCUwCSuWDPAvVobgqBtTjUc-EaSyQacBnZLEAUEEWax9NNq9yqC-cMkaCyDwAj3Ut6/pub?output=csv";
-
-// fetch(API_URL)
-//     .then(res => res.json())
-//     .then(data => {
-//         data.forEach(mentor => {
-//             const row = document.createElement("div");
-//             row.classList.add("mentor-row");
-
-//             row.innerHTML = `
-//                 <img src="assets/mentors/${mentor.Photo_name}" alt="${mentor.Name}">
-                
-//                 <div class="mentor-details">
-//                     <div class="mentor-name">${mentor.Name}</div>
-//                     <div class="mentor-subject">${mentor.Subject}</div>
-//                     <div class="mentor-meta">${mentor.Qualification}</div>
-//                     <div class="mentor-meta">${mentor.Experience}</div>
-//                 </div>
-//             `;
-
-//             mentorsContainer.appendChild(row);
-//         });
-//     })
-//     .catch(err => {
-//         console.error("Error loading mentors:", err);
-//     });
-
-
 
 const mentorsContainer = document.getElementById("mentors-list");
 
